d0f4eb99b7cb1425f58ee942cc7e8acfb4081918
Christian Neff <christian.neff@gmail.com>
2017-10-11 14:12:27 +0200

    Update and fix login

--- portal/index.php
+++ portal/index.php
@@ -1,28 +1,21 @@
 <?php
-  session_start();
-  if ( !is_file("inc/conf.php") )
-  {
-    header("Location: install.php");
-    die("Bitte Konfiguration &uuml;berpr&uuml;fen");
-  }
-  require_once "inc/conf.php";
-/*  if ( is_file("install.php") )
-  {
-    die("Bitte erst die install.php l&ouml;schen");
-  } */
-  if ( !is_file($skrupel_path."inc.conf.php") ) 
-  {
-    header("Location: install.php");
-    die("Bitte Konfiguration &uuml;berpr&uuml;fen");
-  } 
-  
-  require_once $skrupel_path."inc.conf.php";
-  require_once "inc/_db.php";
-  
-  if ($_POST["login"] && $_POST["pass"])
-  {
-    require_once "inc/_db.php";
-    $sql   = "SELECT user.*, spiele.sid
+if (!is_file('inc/conf.php')) {
+    header('Location: install.php');
+    die('Bitte Konfiguration &uuml;berpr&uuml;fen!');
+}
+require_once 'inc/conf.php';
+
+if (!is_file($skrupel_path.'/inc.conf.php')) {
+    header('Location: install.php');
+    die('Bitte Konfiguration &uuml;berpr&uuml;fen');
+}
+require_once $skrupel_path.'/inc.conf.php';
+
+session_start();
+require_once 'inc/_db.php';
+
+if ($_POST['login'] && $_POST['pass']) {
+    $sql = "SELECT user.*, spiele.sid
               FROM $skrupel_user AS user 
               LEFT JOIN $skrupel_spiele AS spiele 
               ON phase = 0 
@@ -36,42 +29,62 @@
                       OR user.id = spieler_8 
                       OR user.id = spieler_9 
                       OR user.id = spieler_10) 
-              WHERE user.nick=\"{$_POST["login"]}\" 
-                    AND user.passwort=\"{$_POST["pass"]}\" 
-              GROUP BY user.id"; 
+              WHERE user.nick=\"{$_POST['login']}\" 
+              GROUP BY user.id";
     $query = mysql_query($sql);
-    if ( mysql_num_rows($query) == 1 )
-    {
-      $data = mysql_fetch_assoc($query);
-if ($data["portal_bann"]!=1) {
-      $_SESSION["user_id"] = $data["id"];
-      $_SESSION["uid"]     = $data["uid"];
-      $_SESSION["sid"]     = $data["sid"];
-      $_SESSION["name"]    = $data["nick"]; 
-      $_SESSION["pass"]    = $_POST["pass"];
-      $_SESSION["mail"] = $data["email"];
-      $_SESSION["layout"] = $data["portal_layout"];
 
-setcookie("mail", $data["email"], time()+$cookie_dauer);
-setcookie("login_name", $data["nick"], time()+$cookie_dauer);
-setcookie("layout", $data["portal_layout"], time()+$cookie_dauer);
+    $login_error = '';
 
-if ($_POST["cookie"]=="on" || $_POST["cookie"]=="true")
-{ setcookie("user", $data["nick"], time()+$cookie_dauer);
-setcookie("password", $_POST["pass"], time()+$cookie_dauer); }
+    if (mysql_num_rows($query) == 1) {
+        $data = mysql_fetch_assoc($query);
 
-$zeiger = mysql_query("INSERT INTO $skrupel_chat (datum,text,von,farbe) VALUES ('".time()."', '".$_POST["login"]." hat sich eingeloggt.', 'System', '000000')");
-    }	else { $login_error="bann"; }
-  }	else { $login_error="wrong_data"; }
+        $passwort_crypt = cryptPasswd($_POST['pass'], $data['salt']);
+        list($passwort, $salt) = explode(':', $passwort_crypt);
+
+        if (strcmp($passwort, $data['passwort']) !== 0) {
+            $login_error = 'wrong_data';
+        } elseif ($data['portal_bann'] == 1) {
+            $login_error = 'bann';
+        }
+    } else {
+        $login_error = 'wrong_data';
+    }
+
+    if ($login_error == '') {
+        $_SESSION['user_id'] = $data['id'];
+        $_SESSION['uid'] = $data['uid'];
+        $_SESSION['sid'] = $data['sid'];
+        $_SESSION['name'] = $data['nick'];
+        //$_SESSION["pass"] = $_POST["pass"];
+        $_SESSION['mail'] = $data['email'];
+        $_SESSION['layout'] = $data['portal_layout'];
+
+        setcookie('mail', $data['email'], time() + $cookie_dauer);
+        setcookie('login_name', $data['nick'], time() + $cookie_dauer);
+        setcookie('layout', $data['portal_layout'], time() + $cookie_dauer);
+
+        if ($_POST['cookie'] == 'on' || $_POST['cookie'] == 'true') {
+            setcookie('user', $data['nick'], time() + $cookie_dauer);
+            //setcookie('password', $_POST['pass'], time() + $cookie_dauer);
+        }
+
+        $zeiger = mysql_query("INSERT INTO $skrupel_chat (datum,text,von,farbe) VALUES ('".time()."', '".$_POST['login']." hat sich eingeloggt.', 'System', '000000')");
+    }
 }
 
-  if (isset($_SESSION["user_id"]) && $_POST["return"] && file_exists($_POST["return"]))
-{ header('Location:'.$_POST["return"]); exit; }
-else if (isset($_SESSION["user_id"]) && $_SERVER["HTTP_REFERER"] && file_exists($_SERVER["HTTP_REFERER"]))
-{ header('Location: '.$_SERVER["HTTP_REFERER"]); exit; }
-else if (isset($_SESSION["user_id"]))
-   { header('Location: active.php'); exit; }
-
-  if (!$_SESSION["user_id"])
-  { require "login.php"; }
- ?>
+if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) {
+    if ($_POST['return'] && file_exists($_POST['return'])) {
+        header('Location: '.$_POST['return']);
+        exit;
+    } else {
+        if ($_SERVER['HTTP_REFERER'] && file_exists($_SERVER['HTTP_REFERER'])) {
+            header('Location: '.$_SERVER['HTTP_REFERER']);
+            exit;
+        } else {
+            header('Location: active.php');
+            exit;
+        }
+    }
+} else {
+    require 'login.php';
+}
--- portal/login.php
+++ portal/login.php
@@ -1,40 +1,43 @@
-<?php 
-  include "inc/initial.php";
-  if ( $_SESSION["user_id"]) 
-  {
-    header("Location: index.php"); 
-  }
-$page_name="Login-Bereich";
-  require_once ("inc/_header.php");
+<?php
+include 'inc/initial.php';
+
+if ($_SESSION['user_id']) {
+    header('Location: index.php');
+}
+
+$page_name = 'Login-Bereich';
+require_once 'inc/_header.php';
 ?>
-<form action="index.php" method="post" name="formular" onSubmit="if(this.login.value=='' || this.pass.value==''){ alert('Nicht alle Felder ausgefüllt!'); return false; }">
-<center>
- <img src="<?php echo $skrupel_path; ?>bilder/logo_login.gif" width="329" height="208" alt="Skrupel Logo" title="" /><br>
- <table border="0" cellspacing="0" cellpadding="4" align="center">
-<?php if (isset($login_error) && $login_error=="wrong_data"){ ?>
-<tr><td /><td style="color:red;">Login-Daten falsch.</td></tr>
-<?php } else if (isset($login_error) && $login_error=="bann"){ ?>
-<tr><td /><td style="color:red;">Dieser Account wurde aus dem Portal gebannt.</td></tr>
-<?php } ?>
-   <tr>     
-     <td align="right">Benutzername&nbsp;</td>
-     <td><input type="text" name="login" maxlength="50" style="width:350px;<?php if(isset($_POST["login"])){ ?> border-color:red; color:red<?php } ?>" value="<?php if (isset($_POST["login"])) { echo $_POST["login"]; } else { echo $HTTP_COOKIE_VARS["login_name"]; } ?>" /></td>     
-    </tr>
-    <tr>
-      <td align="right">Passwort&nbsp;</td>
-      <td><input type="password" name="pass" maxlength="50" style="width:350px;<?php if(isset($_POST["login"])){ ?> border-color:red; color:red<?php } ?>" value="" /></td>      
-    </tr>
-<tr><td /><td align="center"><input type="submit" name="submit" value="Login" style="width:300px;" />
-      </td>      
-    </tr>
-    <tr>
-      <td /><td align="left">
-        <input type="hidden" name="return" value="active.php" />
-<input type="checkbox" name="cookie" /> Bei Seitenbesuchen automatisch einloggen
-        </td></tr>
-  </table>
-</center>
-</form>
-<?php  
-  require_once ("inc/_footer.php");
-?>
+    <center>
+        <img src="<?php echo $skrupel_path; ?>/bilder/logo_login.gif" width="329" height="208" alt="Skrupel Logo" title=""/><br>
+<?php if (isset($login_error) && $login_error == 'wrong_data'): ?>
+        <p class="msg-error">Login-Daten falsch.</p>
+<?php elseif (isset($login_error) && $login_error == 'bann'): ?>
+        <p class="msg-error">Dieser Account wurde aus dem Portal gebannt.</p>
+<?php endif; ?>
+        <form action="index.php" method="post" name="formular" onSubmit="if (this.login.value == '' || this.pass.value == '') { alert('Nicht alle Felder ausgefÃ¼llt!'); return false; }">
+            <table border="0" cellspacing="0" cellpadding="4" align="center">
+                <tr>
+                    <td align="right">Benutzername</td>
+                    <td><input type="text" name="login" maxlength="50" style="width:350px;"<?php if (isset($_POST['login'])): ?> class="form-error"<?php endif; ?> value="<?= isset($_POST['login']) ? $_POST['login'] : $_COOKIE['login_name']; ?>"/></td>
+                </tr>
+                <tr>
+                    <td align="right">Passwort&nbsp;</td>
+                    <td><input type="password" name="pass" maxlength="50" style="width:350px;<?php if (isset($_POST['login'])) { ?> border-color:red; color:red<?php } ?>" value=""/></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="submit" value="Login" style="width:300px;"/></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <label>
+                            <input type="hidden" name="return" value="active.php"/>
+                            <input type="checkbox" name="cookie"/> Bei Seitenbesuchen automatisch einloggen
+                        </label>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+<?php
+require_once 'inc/_footer.php';
